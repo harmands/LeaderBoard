4f7ebbe23c0ce566bafa380276a300291fb57f1f
Harmandeep Singh <harmandeep.singh1@delhivery.com>
2019-05-13 19:53:06 +0530

    Add basic leaderboard code - User's point can be fetched - User's rank can be fetched - User's points can be increased

--- tests/test_leaderboard.py
+++ tests/test_leaderboard.py
@@ -1,15 +1,16 @@
 import unittest
 
-from leaderboard import BST, BSTNode
+from leaderboard import BST, BSTNode, LeaderBoard
+
 
 def inorder(node):
-    def _inorder(node, ans)
+    def _inorder(node, ans):
         if node is None:
             return
 
-        _inorder(node.left)
+        _inorder(node.left, ans)
         ans.append(node.val)
-        _inorder(node.right)
+        _inorder(node.right, ans)
     ans = []
     _inorder(node, ans)
     return ans
@@ -30,29 +31,73 @@ class TestBSTNode(unittest.TestCase):
 class TestBST(unittest.TestCase):
 
     def setUp(self):
+
         self.test_tree = BST()
+        self.test_tree.insert(5)
+        self.test_tree.insert(6)
+        self.test_tree.insert(3)
+        self.test_tree.insert(1)
 
     def test_insert(self):
 
-        self.test_tree.insert(5)
         self.assertEqual(self.test_tree.root.val, 5)
-        self.assertEqual(self.test_tree.root.rank, 0)
+        self.assertEqual(self.test_tree.root.rank, 2)
 
-        self.test_tree.insert(6)
         self.assertEqual(self.test_tree.root.right.val, 6)
         self.assertEqual(self.test_tree.root.right.rank, 0)
 
-        self.test_tree.insert(3)
-        self.assertEqual(self.test_tree.root.left.val, 4)
-        self.assertEqual(self.test_tree.root.left.rank, 0)
-        self.assertEqual(self.test_tree.root.rank, 1)
+        self.assertEqual(self.test_tree.root.left.val, 3)
+        self.assertEqual(self.test_tree.root.left.rank, 1)
 
-        self.test_tree.insert(1)
         self.assertEqual(self.test_tree.root.left.left.val, 1)
-        self.assertEqual(self.test_tree.root.left.leftrank, 0)
+        self.assertEqual(self.test_tree.root.left.left.rank, 0)
         self.assertEqual(self.test_tree.root.rank, 2)
         self.assertEqual(self.test_tree.root.left.rank, 1)
 
-    def test_is_bst(self, node, ans):
+    def test_is_bst(self):
         self.assertEqual(inorder(self.test_tree.root), [1, 3, 5, 6])
 
+    def test_search(self):
+        self.assertEqual(self.test_tree.search(1), 0)
+        self.assertEqual(self.test_tree.search(3), 1)
+        self.assertEqual(self.test_tree.search(5), 2)
+        self.assertEqual(self.test_tree.search(6), 3)
+        self.assertIsNone(self.test_tree.search(9))
+
+    def test_delete(self):
+        self.test_tree.delete(3)
+        self.assertEqual(inorder(self.test_tree.root), [1, 5, 6])
+        self.assertEqual(self.test_tree.search(1), 0)
+        self.assertEqual(self.test_tree.search(5), 1)
+        self.assertEqual(self.test_tree.search(6), 2)
+
+        self.test_tree.delete(5)
+        self.assertEqual(inorder(self.test_tree.root), [1, 6])
+        self.assertEqual(self.test_tree.search(1), 0)
+        self.assertEqual(self.test_tree.search(6), 1)
+
+
+class TestLeaderBoard(unittest.TestCase):
+
+    def setUp(self):
+        self.lb = LeaderBoard()
+
+    def test_increment(self):
+        self.lb.increment(0, 5)
+        self.lb.increment(1, 6)
+        self.lb.increment(0, 2)
+
+        self.assertEqual(self.lb.user_hash[0], 7)
+        self.assertEqual(self.lb.user_hash[1], 6)
+
+    def test_rank_user(self):
+        self.lb.increment(0, 5)
+        self.lb.increment(1, 6)
+
+        self.assertEqual(self.lb.rank_user(0), 0)
+        self.assertEqual(self.lb.rank_user(1), 1)
+
+        self.lb.increment(0, 2)
+
+        self.assertEqual(self.lb.rank_user(0), 1)
+        self.assertEqual(self.lb.rank_user(1), 0)
